9e8dd4755749b7126d68d61e13f3c24b95dcc512
tanersaydam <tanersaydam@gmail.com>
2023-03-13 09:49:58 +0300

    .

---
+++ frontend/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render();
+
+reportWebVitals();
